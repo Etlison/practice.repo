4e9148b43dac9f206e907370666295356f394cc5
etien <etechn@gmail.com>
2022-08-06 01:31:53 +0000

    my first commit

---
+++ tdapp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+name=ngwa
+bank=Afrland
+location=bamenda
+task=login
+
+echo "Please, $name what is the name of the bank"
+echo "oh, thank you, the bank is called $bank"
+echo "and where is $bank located"
+echo "$bank is in $location"
+echo thank you. 
+
+echo Please enter your name
+read name
+echo please enter your password
+read -s 2525
+echo thank you
